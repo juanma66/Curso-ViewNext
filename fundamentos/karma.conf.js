b59129633eb20b559225648b76731daaae7a8b9d
juanma66 <juanma.dg@hotmail.es>
2021-09-27 13:51:28 +0200

    actualizar 2

---
+++ fundamentos/karma.conf.js
@@ -0,0 +1,79 @@
+// Karma configuration
+// Generated on Mon Sep 27 2021 12:51:56 GMT+0200 (hora de verano de Europa central)
+
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'app/js/**/*.js',
+      'spec/**/*.js'
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine',
+      'karma-coverage'
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
+    preprocessors: {
+      'app/js/**/*.js': ['coverage']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: 'coverage/',
+      reporters: [{ type: 'html', subdir: '.' }]
+    },
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser instances should be started simultaneously
+    concurrency: Infinity
+  })
+}
---
+++ fundamentos/spec/denos.spec.js
@@ -0,0 +1,21 @@
+describe('Demo de la prueba',function(){
+
+describe('una anidada', function(){
+
+})
+
+             it('Este funciona siempre', function(){
+
+                expect(true).toBeTruthy()
+             })
+
+             it('Este funciona siempre', function(){
+
+                expect(true).not.toBeTruthy()
+             })
+
+
+})
+   describe('otra suite', function(){
+
+   })
