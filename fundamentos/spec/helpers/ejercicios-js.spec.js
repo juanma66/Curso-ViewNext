a039311c57b4bf8e38b32cdcf68cd5787f1aab56
juanma66 <juanma.dg@hotmail.es>
2021-09-29 10:54:24 +0200

    testing nuevo

--- fundamentos/spec/helpers/ejercicios-js.spec.js
+++ fundamentos/spec/helpers/ejercicios-js.spec.js
@@ -1,14 +1,28 @@
 
-describe('ejer3',function(){
-it('iniciar',function(){
-let arr=iniciarArray(5,0);
-expect(arr.length).toBe(5);
-for(i of arr){
-expect(i).toBe(0);
-}
+    describe('ejer3',function(){
+    it('iniciar',function(){
+    let arr=iniciarArray(5,0);
+    expect(arr.length).toBe(5);
+    for(i of arr){
+    expect(i).toBe(0);
+    }
 
-})
-})
+    })
+    })
+
+
+    describe('ejer4',()=>{
+     it('5 numeros primo',()=>{
+         let vector=[2,3,5,7,11];
+         let cuantos=5;
+
+         let resultado=primos(cuantos);
+
+         expect(resultado).toEqual(vector)
+     })
+
+
+    })
 
 
 describe('ejer6...',()=>{
